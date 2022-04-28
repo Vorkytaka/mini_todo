450eedbf01ce8dd760ac469e84f691a44163982d
Vorkytaka <vorkytaka@gmail.com>
2022-04-29 00:33:19 +0300

    - Update title

--- lib/ui/todo_detailed/todo_detailed_screen.dart
+++ lib/ui/todo_detailed/todo_detailed_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mini_todo/data/repository.dart';
 import 'package:mini_todo/domain/todo/todo_list_cubit.dart';
 import 'package:mini_todo/ui/formatter.dart';
 import 'package:mini_todo/ui/todo_list/todo_list_screen.dart';
@@ -26,31 +27,7 @@ class TodoDetailedScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              SafeArea(
-                bottom: false,
-                child: Material(
-                  elevation: 1,
-                  child: ConstrainedBox(
-                    constraints: const BoxConstraints(minHeight: kToolbarHeight),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        const BackButton(),
-                        const SizedBox(width: 8),
-                        Expanded(
-                          child: Text(
-                            todo.title,
-                            style: Theme.of(context).textTheme.headline6,
-                          ),
-                        ),
-                        TodoCheckbox(todo: todo),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
+              _AppBar(todo: todo),
               if (todo.date != null)
                 ListTile(
                   leading: const Icon(Icons.today),
@@ -61,7 +38,7 @@ class TodoDetailedScreen extends StatelessWidget {
               else
                 ListTile(
                   leading: const Icon(Icons.today),
-                  title: Text('Без даты'),
+                  title: const Text('Без даты'),
                   onTap: () {},
                   dense: true,
                 ),
@@ -75,13 +52,77 @@ class TodoDetailedScreen extends StatelessWidget {
               else
                 ListTile(
                   leading: const Icon(Icons.access_time_outlined),
-                  title: Text('Без времени'),
+                  title: const Text('Без времени'),
                   onTap: () {},
                   dense: true,
                 ),
             ],
           );
         },
+      ),
+    );
+  }
+}
+
+class _AppBar extends StatefulWidget {
+  final Todo todo;
+
+  const _AppBar({
+    Key? key,
+    required this.todo,
+  }) : super(key: key);
+
+  @override
+  State<_AppBar> createState() => _AppBarState();
+}
+
+class _AppBarState extends State<_AppBar> {
+  late final TextEditingController _titleController;
+
+  @override
+  void initState() {
+    super.initState();
+    _titleController = TextEditingController(text: widget.todo.title);
+    _titleController.addListener(() {
+      context.read<Repository>().update(widget.todo.copyWith(title: _titleController.text));
+    });
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      bottom: false,
+      child: Material(
+        elevation: 1,
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(minHeight: kToolbarHeight),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const BackButton(),
+              const SizedBox(width: 8),
+              Expanded(
+                child: TextField(
+                  controller: _titleController,
+                  decoration: const InputDecoration(border: InputBorder.none),
+                  style: Theme.of(context).textTheme.headline6,
+                  maxLines: null,
+                  keyboardType: TextInputType.text,
+                  textInputAction: TextInputAction.done,
+                ),
+              ),
+              TodoCheckbox(todo: widget.todo),
+            ],
+          ),
+        ),
       ),
     );
   }
