08403156005d2ef27c421396e295266bee366a2e
Vorkytaka <vorkytaka@gmail.com>
2022-05-28 02:17:01 +0300

    - Notification icon - Add crashlytics

--- lib/generated/intl/messages_ru.dart
+++ lib/generated/intl/messages_ru.dart
@@ -21,46 +21,71 @@ class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'ru';
 
   final messages = _notInlinedMessages(_notInlinedMessages);
-
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
         "common__all": MessageLookupByLibrary.simpleMessage("Все"),
         "common__cancel": MessageLookupByLibrary.simpleMessage("Отмена"),
-        "common__completed": MessageLookupByLibrary.simpleMessage("Выполненные"),
+        "common__completed":
+            MessageLookupByLibrary.simpleMessage("Выполненные"),
         "common__confirm": MessageLookupByLibrary.simpleMessage("Принять"),
-        "common__hide_completed": MessageLookupByLibrary.simpleMessage("Скрыть выполненные"),
+        "common__hide_completed":
+            MessageLookupByLibrary.simpleMessage("Скрыть выполненные"),
         "common__inbox": MessageLookupByLibrary.simpleMessage("Входящие"),
         "common__no": MessageLookupByLibrary.simpleMessage("Нет"),
-        "common__show_completed": MessageLookupByLibrary.simpleMessage("Показать выполненные"),
+        "common__show_completed":
+            MessageLookupByLibrary.simpleMessage("Показать выполненные"),
         "common__today": MessageLookupByLibrary.simpleMessage("Сегодня"),
         "common__tomorrow": MessageLookupByLibrary.simpleMessage("Завтра"),
         "common__yes": MessageLookupByLibrary.simpleMessage("Да"),
         "common__yesterday": MessageLookupByLibrary.simpleMessage("Вчера"),
         "datetime_dialog__time": MessageLookupByLibrary.simpleMessage("Время"),
-        "delete_folder_dialog__content": MessageLookupByLibrary.simpleMessage("Вы точно хотите удалить папку "),
+        "delete_folder_dialog__content": MessageLookupByLibrary.simpleMessage(
+            "Вы точно хотите удалить папку "),
         "delete_folder_dialog__delete_todos_toggle":
             MessageLookupByLibrary.simpleMessage("Удалить все задачи из папки"),
-        "delete_folder_dialog__title": MessageLookupByLibrary.simpleMessage("Удалить папку?"),
-        "delete_todo_dialog__caution": MessageLookupByLibrary.simpleMessage("Это действие нельзя отменить."),
-        "delete_todo_dialog__content": MessageLookupByLibrary.simpleMessage("Вы точно хотите удалить "),
-        "delete_todo_dialog__title": MessageLookupByLibrary.simpleMessage("Удалить задачу?"),
-        "editable_folder_dialog__hint": MessageLookupByLibrary.simpleMessage("Название папки"),
-        "editable_folder_dialog__title_error": MessageLookupByLibrary.simpleMessage("Введите название папки"),
+        "delete_folder_dialog__title":
+            MessageLookupByLibrary.simpleMessage("Удалить папку?"),
+        "delete_todo_dialog__caution": MessageLookupByLibrary.simpleMessage(
+            "Это действие нельзя отменить."),
+        "delete_todo_dialog__content":
+            MessageLookupByLibrary.simpleMessage("Вы точно хотите удалить "),
+        "delete_todo_dialog__title":
+            MessageLookupByLibrary.simpleMessage("Удалить задачу?"),
+        "editable_folder_dialog__hint":
+            MessageLookupByLibrary.simpleMessage("Название папки"),
+        "editable_folder_dialog__title_error":
+            MessageLookupByLibrary.simpleMessage("Введите название папки"),
         "folder__today": MessageLookupByLibrary.simpleMessage("Сегодня"),
-        "folder_list_screen__create_folder_tooltip": MessageLookupByLibrary.simpleMessage("Создать папку"),
-        "folder_list_screen__create_todo_tooltip": MessageLookupByLibrary.simpleMessage("Создать задачу"),
-        "folder_screen__delete_folder": MessageLookupByLibrary.simpleMessage("Удалить папку"),
-        "folder_screen__empty_caution": MessageLookupByLibrary.simpleMessage("Не ленись, добавь первую задачу!"),
-        "folder_screen__empty_title": MessageLookupByLibrary.simpleMessage("Здесь пусто"),
-        "folder_screen__update_folder": MessageLookupByLibrary.simpleMessage("Изменить папку"),
-        "new_todo_dialog__title_hint": MessageLookupByLibrary.simpleMessage("Задача"),
-        "new_todo_dialog__title_required": MessageLookupByLibrary.simpleMessage("Введите название задачи"),
-        "select_folder_dialog__title": MessageLookupByLibrary.simpleMessage("Выберите папку"),
-        "today_screen__overdue": MessageLookupByLibrary.simpleMessage("Просроченные"),
-        "todo_detailed_screen__add_subtodo": MessageLookupByLibrary.simpleMessage("Добавить подзадачу"),
-        "todo_detailed_screen__delete": MessageLookupByLibrary.simpleMessage("Удалить"),
-        "todo_detailed_screen__no_date": MessageLookupByLibrary.simpleMessage("Без даты"),
-        "todo_detailed_screen__no_time": MessageLookupByLibrary.simpleMessage("Без времени"),
-        "todo_detailed_screen__note_hint": MessageLookupByLibrary.simpleMessage("Заметка"),
-        "todo_detailed_screen__subtodo_hint": MessageLookupByLibrary.simpleMessage("Подзадача")
+        "folder_list_screen__create_folder_tooltip":
+            MessageLookupByLibrary.simpleMessage("Создать папку"),
+        "folder_list_screen__create_todo_tooltip":
+            MessageLookupByLibrary.simpleMessage("Создать задачу"),
+        "folder_screen__delete_folder":
+            MessageLookupByLibrary.simpleMessage("Удалить папку"),
+        "folder_screen__empty_caution": MessageLookupByLibrary.simpleMessage(
+            "Не ленись, добавь первую задачу!"),
+        "folder_screen__empty_title":
+            MessageLookupByLibrary.simpleMessage("Здесь пусто"),
+        "folder_screen__update_folder":
+            MessageLookupByLibrary.simpleMessage("Изменить папку"),
+        "new_todo_dialog__title_hint":
+            MessageLookupByLibrary.simpleMessage("Задача"),
+        "new_todo_dialog__title_required":
+            MessageLookupByLibrary.simpleMessage("Введите название задачи"),
+        "select_folder_dialog__title":
+            MessageLookupByLibrary.simpleMessage("Выберите папку"),
+        "today_screen__overdue":
+            MessageLookupByLibrary.simpleMessage("Просроченные"),
+        "todo_detailed_screen__add_subtodo":
+            MessageLookupByLibrary.simpleMessage("Добавить подзадачу"),
+        "todo_detailed_screen__delete":
+            MessageLookupByLibrary.simpleMessage("Удалить"),
+        "todo_detailed_screen__no_date":
+            MessageLookupByLibrary.simpleMessage("Без даты"),
+        "todo_detailed_screen__no_time":
+            MessageLookupByLibrary.simpleMessage("Без времени"),
+        "todo_detailed_screen__note_hint":
+            MessageLookupByLibrary.simpleMessage("Заметка"),
+        "todo_detailed_screen__subtodo_hint":
+            MessageLookupByLibrary.simpleMessage("Подзадача")
       };
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'dart:io';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_native_timezone/flutter_native_timezone.dart';
+import 'package:mini_todo/firebase_options.dart';
 import 'package:timezone/data/latest_all.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
 
@@ -12,9 +15,12 @@ import 'app.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
   await _configureLocalTimeZone();
 
-  const initSettings = InitializationSettings(android: AndroidInitializationSettings('@mipmap/ic_launcher'));
+  const initSettings = InitializationSettings(android: AndroidInitializationSettings('notification_icon'));
   final notificationPlugin = FlutterLocalNotificationsPlugin();
   await notificationPlugin.initialize(initSettings);
 
